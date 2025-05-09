6dc7e8ba5c0c9af12cfc9711973552c82e05682f
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2025-05-09 18:05:55 +0700

    fixed PORT

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
-const PORT = 3100;
+const PORT =  process.env.PORT || 3100;
 
 const TMDB_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmMwNTMyNmQwNWRkMGNhYTE4ZjAzMTRiNWJhZWZjOSIsIm5iZiI6MTcwMzA2NTA1MS4wNzUsInN1YiI6IjY1ODJiNWRiZTgxMzFkNDE0N2E0YWJhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s-5yc6_ze6KO1V6uNeFzI8EFNTCGAryr4V4acKeQFfQ";
